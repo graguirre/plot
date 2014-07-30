83826adb89c2cac9a6a1fac367e22df7b481e6f5
graguirre <graguirre@gmail.com>
2014-07-30 12:36:00 +0200

    first commit

---
+++ orca-plot.py
@@ -0,0 +1,81 @@
+#
+# Plot ORCA SCF iterations
+#
+# Gonzalo Aguirre <gaguirre@gmail.com>
+# Version: 0.1
+#
+
+import time
+import numpy as np
+import matplotlib.pyplot as plt
+import sys, getopt
+
+def usage():
+	print >> sys.stderr, "Options:"
+    	print >> sys.stderr, "    -h                Show help"
+    	print >> sys.stderr, "    -i <input_file>   Input file"
+    	print >> sys.stderr, "Syntax: $ python2 plot-orca.py -i <input-file>"
+    	sys.exit(1)
+
+def plot(f):
+	# initialize variables
+	i = 0		# iteration accumulator
+	E = []		# energy vector
+	read_on = 0	# flag SCF iterations 
+	
+	plt.ion()
+	plt.show()
+	
+	fig = plt.figure()
+	for line in f:
+		# inicio iteraciones
+		if line.find('ITERATIONS') > 0:  
+			read_on = 1
+			#fig = plt.figure()
+		# final iteraciones
+		elif line.find('SUCCESS') > 0:
+			read_on = 0
+		elif read_on == 1:
+			lline = line.split()
+			if len(lline)>0 and lline[0].isdigit() :
+				E.append(float(lline[1]))
+				print i, lline[0], lline[1] # points to plot (It Energy(Eh))
+				plt.axis([max([i-10,0]),i,min(E),max(E)])
+				plt.scatter(float(i),float(lline[1]))
+				# print x, y
+				plt.draw()
+				time.sleep(.1)
+				i += 1 # increment iteration
+
+
+def main(argv):
+	inputfile=''
+	
+	try:
+		opts, args = getopt.getopt(argv,"hi:")
+	except getopt.GetoptError:
+		usage()
+		sys.exit(1)
+
+	for opt, arg in opts:
+		if opt == '-h':
+			usage()
+			sys.exit(0)
+		elif opt == '-i':
+			inputfile = arg
+	
+	if inputfile == '':
+		usage()
+		sys.exit(3)
+
+	try:
+		f = open(inputfile,'r')
+    	except IOError:
+        	print >> sys.stderr, "File "+ inputfile +" not found"
+        	sys.exit(2)
+
+	plot(f)
+
+
+if __name__ == "__main__":
+	main(sys.argv[1:])
