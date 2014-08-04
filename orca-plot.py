49f9d4c8b29c35ca501cbd97f14d3bc98c840607
graguirre <graguirre@gmail.com>
2014-08-04 18:17:46 +0200

    new features added output example

--- orca-plot.py
+++ orca-plot.py
@@ -13,25 +13,29 @@ import sys, getopt
 def usage():
 	print >> sys.stderr, "Options:"
     	print >> sys.stderr, "    -h                Show help"
+    	print >> sys.stderr, "    -d                Dump mode, don't plot"
     	print >> sys.stderr, "    -i <input_file>   Input file"
+    	print >> sys.stderr, "    -t <seconds>      Step time (default 0.1)"
+    	print >> sys.stderr, "    -l <number>       Iterations to show (default 10)"
     	print >> sys.stderr, "Syntax: $ python2 plot-orca.py -i <input-file>"
     	sys.exit(1)
 
-def plot(f):
+def plot(f,t,L,d):
 	# initialize variables
 	i = 0		# iteration accumulator
 	E = []		# energy vector
 	read_on = 0	# flag SCF iterations 
 	
-	plt.ion()
-	plt.show()
+        if not d:
+	    plt.ion()
+	    plt.show()
 	
-	fig = plt.figure()
+	    fig = plt.figure()
+
 	for line in f:
 		# inicio iteraciones
 		if line.find('ITERATIONS') > 0:  
 			read_on = 1
-			#fig = plt.figure()
 		# final iteraciones
 		elif line.find('SUCCESS') > 0:
 			read_on = 0
@@ -39,31 +43,41 @@ def plot(f):
 			lline = line.split()
 			if len(lline)>0 and lline[0].isdigit() :
 				E.append(float(lline[1]))
-				print i, lline[0], lline[1] # points to plot (It Energy(Eh))
-				plt.axis([max([i-10,0]),i,min(E),max(E)])
-				plt.scatter(float(i),float(lline[1]))
-				# print x, y
-				plt.draw()
-				time.sleep(.1)
+                                if d:
+				    print i, lline[0], lline[1] # points to plot (It Energy(Eh))
+                                else:
+				    plt.axis([max([i-L,0]),i,min(E),max(E)])
+				    plt.scatter(float(i),float(lline[1]))
+				    plt.draw()
+				    time.sleep(float(t))
 				i += 1 # increment iteration
 
 
 def main(argv):
 	inputfile=''
+        sleeptime=0.1
+        showlast=10
+        dumpmode=0
 	
 	try:
-		opts, args = getopt.getopt(argv,"hi:")
+            opts, args = getopt.getopt(argv,"hdi:t:l:")
 	except getopt.GetoptError:
-		usage()
-		sys.exit(1)
+	    usage()
+	    sys.exit(1)
 
 	for opt, arg in opts:
 		if opt == '-h':
 			usage()
 			sys.exit(0)
 		elif opt == '-i':
-			inputfile = arg
-	
+			inputfile = arg	
+		elif opt == '-t':
+			sleeptime = arg
+		elif opt == '-l':
+			showlast = int(arg)
+		elif opt == '-d':
+			dumpmode = 1
+
 	if inputfile == '':
 		usage()
 		sys.exit(3)
@@ -74,7 +88,7 @@ def main(argv):
         	print >> sys.stderr, "File "+ inputfile +" not found"
         	sys.exit(2)
 
-	plot(f)
+	plot(f,sleeptime,showlast,dumpmode)
 
 
 if __name__ == "__main__":
